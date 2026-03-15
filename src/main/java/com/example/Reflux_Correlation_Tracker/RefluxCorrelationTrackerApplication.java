24a1a96d534e983521bf94e32d8b6251fc28157c
YuNam Lee <qw705@naver.com>
2026-03-15 22:21:28 +0900

    Fresh start with Java 21

---
+++ src/main/java/com/example/Reflux_Correlation_Tracker/RefluxCorrelationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reflux_Correlation_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefluxCorrelationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefluxCorrelationTrackerApplication.class, args);
+	}
+
+}
